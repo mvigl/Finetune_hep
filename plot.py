acdf799894267781614510454e600ffecf4fc0a5
mvigl <mavigl@odslserv01.mppmu.mpg.de>
2023-05-22 11:49:13 +0200

    new plot features

--- plot.py
+++ plot.py
@@ -9,39 +9,69 @@ from matplotlib.pyplot import figure
 import torch
 import torch.nn as nn
 import yaml
+from sklearn.metrics import roc_curve, auc
+from matplotlib.pyplot import cm
+import numpy as np
+import argparse
 
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('--lr', type=float,  help='learning rate',default='0.00004')
+parser.add_argument('--bs', type=int,  help='learning rate',default='512')
+parser.add_argument('--ep', type=int,  help='learning rate',default='30')
+parser.add_argument('--mess', help='message',default='')
+parser.add_argument('--Ntrainings', type=int, help='Ntrainings',default='10')
+
+args = parser.parse_args()
+
+learning_rate = args.lr
+batch_size = args.bs
+epochs = args.ep
+message = args.mess
+Ntrainings = args.Ntrainings
 
 hyper_params = {
-   "learning_rate": 0.00004,
-   "steps": 35,
-   "batch_size": 512,
+   "learning_rate": learning_rate,
+   "steps": epochs,
+   "batch_size": batch_size,
 }
+
+X_pfo_test , X_jet_test , njets_test , labels_test , X_label_test , evts_test  = df.get_test_data('/home/iwsatlas1/mavigl/Hbb/ParT/Dataset')
+
+test = df.build_features_and_labels(X_pfo_test[evts_test][:,:2],X_jet_test[evts_test][:,:2],X_label_test[evts_test][:,:2],2)
+
+fpr=[]
+tpr=[]
+threshold=[]
+models = [f'/home/iwsatlas1/mavigl/Finetune_hep/run/21_may_sigma/models/COMBINED_TRAINING_{hyper_params["learning_rate"]}_{hyper_params["batch_size"]}_training_{i}.pt' for i in range(Ntrainings)]
 
 device = df.get_device()
 with open('/home/iwsatlas1/mavigl/Finetune_hep/source/config/myJetClass_full.yaml') as file:
     data_config = yaml.load(file, Loader=yaml.FullLoader)
 model = ParT_mlp.get_model(data_config,for_inference=False)    
 model.to(device)
-model.eval()
 
-model_params_path=(f'/home/iwsatlas1/mavigl/Finetune_hep/source/models/COMBINED_TRAINING_{hyper_params["learning_rate"]}_{hyper_params["batch_size"]}.pt')
-model.load_state_dict(torch.load(model_params_path))
+for mod in models:
+    model.eval()
+    model_params_path=mod
+    model.load_state_dict(torch.load(model_params_path))
 
-X_pfo_test , X_jet_test , njets_test , labels_test , X_label_test , evts_test  = df.get_test_data('/home/iwsatlas1/mavigl/Hbb/ParT/Dataset')
-test = df.build_features_and_labels(X_pfo_test[evts_test][:,:2],X_jet_test[evts_test][:,:2],X_label_test[evts_test][:,:2],2)
-yi_ParT_test = df.get_preds(model,test,evts_test,device)
+    yi = df.get_preds(model,test,evts_test,device)
+    fpr_i, tpr_i, threshold_i = roc_curve(labels_test, yi)
+    fpr.append(fpr_i)
+    tpr.append(tpr_i)
+    threshold.append(threshold_i)
 
-from sklearn.metrics import roc_curve, auc
-disc_ParT = yi_ParT_test
-fpr_ParT, tpr_ParT, threshold_ParT = roc_curve(labels_test, disc_ParT)
 # plot ROC curve
+color = iter(cm.rainbow(np.linspace(0, 1, len(tpr))))
 plt.figure()
 start=0
-plt.plot(tpr_ParT[start:], 1/fpr_ParT[start:], lw=0.8, label="hl feats + Part_Xbb , AUC = {:.2f}%".format(auc(fpr_ParT,tpr_ParT)*100), color = 'orange')
+for i in range(len(tpr)):
+    plt.plot(tpr[i], 1/fpr[i], lw=0.8, label="AUC_{:.0f} = {:.3f}%".format(i,auc(fpr[i],tpr[i])*100), color=next(color))
 plt.xlabel(r'Signal Efficiency')
 plt.semilogy()
 plt.xlim(0,1)
 plt.grid(True)
 plt.legend(loc='lower left')
+plt.title(f"ROC_COMBINED_TRAINING_{ hyper_params['learning_rate'] }_{hyper_params['batch_size']}")
 plt.show()
 plt.savefig(f"ROC_COMBINED_TRAINING_{ hyper_params['learning_rate'] }_{hyper_params['batch_size']}.png")
