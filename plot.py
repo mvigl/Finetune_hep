fe8462482ce62c5950a328b486fa9b2054eba237
mvigl <matthias.vigl@cern.ch>
2023-05-26 10:18:12 +0200

    update

--- plot.py
+++ plot.py
@@ -32,7 +32,7 @@ with open(config_path) as file:
 ParTevent_model = ParT_mlp.get_model(data_config,for_inference=False)  
 ParTevent_test = df.build_features_and_labels(X_pfo_test[evts_test][:,:2],X_jet_test[evts_test][:,:2],X_label_test[evts_test][:,:2],2)
 ParTevent_model.to(device)
-ParTevent_model.load_state_dict(torch.load('../../Finetune_hep/models/ParTevent/ParTevent_hl6_nodes128_nj2_lr4e-05_bs512_WparT_training_0.pt'))
+
 
 mlpXbb_test = df.get_mlp_feat(X_jet_test[evts_test],2,'mlpXbb',evts_test,'../../Finetune_hep/models/ParTXbb/ParTXbb_scores_test.npy',subset)
 mlpHlXbb_test = df.get_mlp_feat(X_jet_test[evts_test],2,'mlpHlXbb',evts_test,'../../Finetune_hep/models/ParTXbb/ParTXbb_scores_test.npy',subset)
@@ -72,13 +72,19 @@ for mod,test in zip(models,test_set):
     print(m)
     mod.eval()
     if models_name[m] == 'ParTevent':
-        yi = ParT_mlp.get_preds(mod,test,evts_test,device)
+        for i in range(5):
+            mod.load_state_dict(torch.load(f'../../Finetune_hep/models/ParTevent/ParTevent_hl3_nodes128_nj2_lr4e-05_bs512_WparT_training_{i}.pt'))
+            yi = ParT_mlp.get_preds(mod,test,evts_test,device)
+            fpr_i, tpr_i, threshold_i = roc_curve(labels_test, yi)
+            fpr.append(fpr_i)
+            tpr.append(tpr_i)
+            threshold.append(threshold_i)
     else: 
         yi = Mlp.get_preds(mod,test,evts_test,device)    
-    fpr_i, tpr_i, threshold_i = roc_curve(labels_test, yi)
-    fpr.append(fpr_i)
-    tpr.append(tpr_i)
-    threshold.append(threshold_i)
+        fpr_i, tpr_i, threshold_i = roc_curve(labels_test, yi)
+        fpr.append(fpr_i)
+        tpr.append(tpr_i)
+        threshold.append(threshold_i)
     m+=1
 
 # plot ROC curve
@@ -86,7 +92,7 @@ color = iter(cm.rainbow(np.linspace(0, 1, len(tpr))))
 plt.figure()
 start=0
 for i in range(len(tpr)):
-    plt.plot(tpr[i], 1/fpr[i], lw=0.8, label=(f'{models_name[i]} ' + "AUC = {:.3f}%".format(auc(fpr[i],tpr[i])*100)), color=next(color))
+    plt.plot(tpr[i], 1/fpr[i], lw=0.8, label=(f'{models_name[i-4] if i>4 else models_name[0]} ' + "AUC = {:.3f}%".format(auc(fpr[i],tpr[i])*100)), color=next(color))
 plt.xlabel(r'Signal Efficiency')
 plt.semilogy()
 plt.xlim(0,1)
@@ -100,7 +106,7 @@ color = iter(cm.rainbow(np.linspace(0, 1, len(tpr))))
 plt.figure()
 start=0
 for i in range(len(tpr)):
-    plt.plot(tpr[i], 1/fpr[i], lw=0.8, label=(f'{models_name[i]} ' + "AUC = {:.3f}%".format(auc(fpr[i],tpr[i])*100)), color=next(color))
+    plt.plot(tpr[i], 1/fpr[i], lw=0.8, label=(f'{models_name[i-4] if i>4 else models_name[0]} ' + "AUC = {:.3f}%".format(auc(fpr[i],tpr[i])*100)), color=next(color))
 plt.xlabel(r'Signal Efficiency')
 plt.semilogy()
 plt.xlim(0.8,1)
