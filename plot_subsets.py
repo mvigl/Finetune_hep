156445dc069d24f86acb91d9f72030852ba0f927
mvigl <matthias.vigl@cern.ch>
2023-09-30 14:53:07 +0200

    subsets

--- plot_subsets.py
+++ plot_subsets.py
@@ -29,13 +29,13 @@ for i in range(len(sizes)):
             filename = line.strip()
             print('reading : ',filename)
             data_index = filename.index("Data")
-            filename = filename[data_index:]
-            filename = f'/raven/u/mvigl/Finetune_hep_dir/Finetune_hep/models/subsets/ParTevent/{sizes[i]}/{filename}'
-            with h5py.File(filename, 'r') as Data:
+            sample_name = filename[data_index:]
+            name = f'/raven/u/mvigl/Finetune_hep_dir/Finetune_hep/models/subsets/ParTevent/{sizes[i]}/{sample_name}'
+            with h5py.File(name, 'r') as Data:
                 yi_ParTevent.append(Data['evt_score'][:].reshape(-1))
                 target_ParTevent.append(Data['evt_label'][:].reshape(-1))
-            filename = f'/raven/u/mvigl/Finetune_hep_dir/Finetune_hep/models/subsets/mlHlXbb/{sizes[i]}/{filename}'
-            with h5py.File(filename, 'r') as Data:    
+            name = f'/raven/u/mvigl/Finetune_hep_dir/Finetune_hep/models/subsets/mlHlXbb/{sizes[i]}/{sample_name}'
+            with h5py.File(name, 'r') as Data:    
                 yi_mlpHlXbb.append(Data['evt_score'][:].reshape(-1))
                 target_mlpHlXbb.append(Data['evt_label'][:].reshape(-1))
     acc_ete.append(balanced_accuracy_score(target_ParTevent.reshape(-1),(yi_ParTevent.reshape(-1)>= 0.5).astype(int)))  
