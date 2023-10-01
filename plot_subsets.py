ad715f4d5ad8c9d70b097c9885b4ba6f5906eede
mvigl <matthias.vigl@cern.ch>
2023-10-02 00:36:27 +0200

    subsets

--- plot_subsets.py
+++ plot_subsets.py
@@ -89,11 +89,12 @@ for i in range(len(sizes)):
             with h5py.File(name, 'r') as Data:    
                 yi_mlpHlXbb.append(Data['evt_score'][:].reshape(-1))
                 target_mlpHlXbb.append(Data['evt_label'][:].reshape(-1))
-    #        name = f'/raven/u/mvigl/Finetune_hep_dir/Finetune_hep/models/subsets/ParTevent_scratch/{sizes[i]}/{sample_name}'
 
-    #        with h5py.File(name, 'r') as Data:
-    #            yi_ParTevent_scratch.append(Data['evt_score'][:].reshape(-1))
-    #            target_ParTevent_scratch.append(Data['evt_label'][:].reshape(-1))      
+            name = f'/raven/u/mvigl/Finetune_hep_dir/Finetune_hep/models/subsets/ParTevent_scratch/{sizes[i]}/{sample_name}'
+            with h5py.File(name, 'r') as Data:
+                yi_ParTevent_scratch.append(Data['evt_score'][:].reshape(-1))
+                target_ParTevent_scratch.append(Data['evt_label'][:].reshape(-1))      
+
     #name = f'/raven/u/mvigl/Finetune_hep_dir/Finetune_hep/models/subsets/mlpLatent/{sizes_latent[i]}/test_mlpLatent.h5'
 
     #with h5py.File(name, 'r') as Data:    
@@ -112,11 +113,11 @@ for i in range(len(sizes)):
     optimal_threshold = thresholds[np.argmax(tpr - fpr)]
     acc_ete.append(balanced_accuracy_score(target_ParTevent,(yi_ParTevent>= optimal_threshold).astype(int)))
 
-    #target_ParTevent_scratch = np.concatenate(target_ParTevent_scratch).reshape(-1)
-    #yi_ParTevent_scratch = np.concatenate(yi_ParTevent_scratch).reshape(-1)
-    #fpr, tpr, thresholds = roc_curve(target_ParTevent_scratch,yi_ParTevent_scratch)
-    #optimal_threshold = thresholds[np.argmax(tpr - fpr)]
-    #acc_ete_scratch.append(balanced_accuracy_score(target_ParTevent_scratch,(yi_ParTevent_scratch>= optimal_threshold).astype(int)))  
+    target_ParTevent_scratch = np.concatenate(target_ParTevent_scratch).reshape(-1)
+    yi_ParTevent_scratch = np.concatenate(yi_ParTevent_scratch).reshape(-1)
+    fpr, tpr, thresholds = roc_curve(target_ParTevent_scratch,yi_ParTevent_scratch)
+    optimal_threshold = thresholds[np.argmax(tpr - fpr)]
+    acc_ete_scratch.append(balanced_accuracy_score(target_ParTevent_scratch,(yi_ParTevent_scratch>= optimal_threshold).astype(int)))  
 
     target_mlpHlXbb = np.concatenate(target_mlpHlXbb).reshape(-1)
     yi_mlpHlXbb = np.concatenate(yi_mlpHlXbb).reshape(-1)    
@@ -126,17 +127,17 @@ for i in range(len(sizes)):
 
 acc_ete = np.array(acc_ete).reshape(-1)
 acc_mlpHlXbb = np.array(acc_mlpHlXbb).reshape(-1)
-#acc_ete_scratch = np.array(acc_ete_scratch).reshape(-1)
+acc_ete_scratch = np.array(acc_ete_scratch).reshape(-1)
 #acc_mlpLatent = np.array(acc_mlpLatent).reshape(-1)
 fig = plt.figure()
 ax = fig.add_subplot(4,1,(1,3))
 ax.plot(sizes, acc_ete, lw=0.8, label=f'E2e',color='b',marker='o')
 #ax.plot(sizes_latent, acc_mlpLatent, lw=0.8, label=f'mlpHlXbb',color='c',marker='o')
 ax.plot(sizes, acc_mlpHlXbb, lw=0.8, label=f'mlpHlXbb',color='r',marker='o')
-#ax.plot(sizes, acc_ete_scratch, lw=0.8, label=f'E2e',color='g',marker='o')
+ax.plot(sizes, acc_ete_scratch, lw=0.8, label=f'E2e',color='g',marker='o')
 ax.set_ylabel(r'balanced_accuracy_score')
 ax.semilogx()
-ax.set_ylim(np.min(acc_mlpHlXbb)-0.001,1)
+ax.set_ylim(np.min(acc_ete_scratch)-0.001,1)
 ax.grid(True)
 ax.legend(loc='upper left')
 ax.set_xlabel(r'Signal efficiency',loc="right")
