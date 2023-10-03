0863dcdac2fe72ea62311cb141e60cccc413f3d5
mavigl <matthias.vigl@cern.ch>
2023-10-03 23:04:57 +0200

    subsets

--- plot_subsets.py
+++ plot_subsets.py
@@ -93,19 +93,19 @@ for i in range(len(sizes)):
             name = f'/raven/u/mvigl/Finetune_hep_dir/Finetune_hep/models/subsets/ParTevent_scratch/{sizes[i]}/{sample_name}'
             with h5py.File(name, 'r') as Data:
                 yi_ParTevent_scratch.append(Data['evt_score'][:].reshape(-1))
-                target_ParTevent_scratch.append(Data['evt_label'][:].reshape(-1))      
+                target_ParTevent_scratch.append(Data['evt_label'][:].reshape(-1))
 
-    #name = f'/raven/u/mvigl/Finetune_hep_dir/Finetune_hep/models/subsets/mlpLatent/{sizes_latent[i]}/test_mlpLatent.h5'
+            if i < len(sizes_latent):
+                name = f'/raven/u/mvigl/Finetune_hep_dir/Finetune_hep/models/subsets/mlpLatent/{sizes_latent[i]}/{sample_name}'
+                with h5py.File(name, 'r') as Data:
+                    yi_ParTevent_scratch.append(Data['evt_score'][:].reshape(-1))
+                    target_ParTevent_scratch.append(Data['evt_label'][:].reshape(-1))          
 
-    #with h5py.File(name, 'r') as Data:    
-    #    yi_mlpLatent.append(Data['evt_score'][:].reshape(-1))
-    #    target_mlpLatent.append(Data['evt_label'][:].reshape(-1))    
-
-    #target_mlpLatent = np.array(target_mlpLatent).reshape(-1)
-    #yi_mlpLatent = np.array(yi_mlpLatent).reshape(-1)    
-    #fpr, tpr, thresholds = roc_curve(target_mlpLatent,yi_mlpLatent)
-    #optimal_threshold = thresholds[np.argmax(tpr - fpr)]    
-    #acc_mlpLatent.append(balanced_accuracy_score(target_mlpLatent,(yi_mlpLatent>= optimal_threshold).astype(int)))   
+    target_mlpLatent = np.concatenate(target_mlpLatent).reshape(-1)
+    yi_mlpLatent = np.concatenate(yi_mlpLatent).reshape(-1)    
+    fpr, tpr, thresholds = roc_curve(target_mlpLatent,yi_mlpLatent)
+    optimal_threshold = thresholds[np.argmax(tpr - fpr)]    
+    acc_mlpLatent.append(balanced_accuracy_score(target_mlpLatent,(yi_mlpLatent>= optimal_threshold).astype(int)))   
 
     target_ParTevent = np.concatenate(target_ParTevent).reshape(-1)
     yi_ParTevent = np.concatenate(yi_ParTevent).reshape(-1)
@@ -128,18 +128,18 @@ for i in range(len(sizes)):
 acc_ete = np.array(acc_ete).reshape(-1)
 acc_mlpHlXbb = np.array(acc_mlpHlXbb).reshape(-1)
 acc_ete_scratch = np.array(acc_ete_scratch).reshape(-1)
-#acc_mlpLatent = np.array(acc_mlpLatent).reshape(-1)
+acc_mlpLatent = np.array(acc_mlpLatent).reshape(-1)
 fig = plt.figure()
 ax = fig.add_subplot(4,1,(1,3))
-ax.plot(sizes, acc_ete, lw=0.8, label=f'E2e',color='b',marker='o')
-#ax.plot(sizes_latent, acc_mlpLatent, lw=0.8, label=f'mlpHlXbb',color='c',marker='o')
-ax.plot(sizes, acc_mlpHlXbb, lw=0.8, label=f'mlpHlXbb',color='r',marker='o')
-ax.plot(sizes, acc_ete_scratch, lw=0.8, label=f'E2e',color='g',marker='o')
+ax.plot(sizes, acc_ete, lw=0.2, label=f'E2e',color='b',marker='.')
+ax.plot(sizes_latent, acc_mlpLatent, lw=0.2, label=f'E2e Frozen',color='c',marker='.')
+ax.plot(sizes, acc_mlpHlXbb, lw=0.2, label=f'mlpHlXbb',color='r',marker='.')
+ax.plot(sizes, acc_ete_scratch, lw=0.2, label=f'E2e scratch',color='g',marker='.')
 ax.set_ylabel(r'balanced_accuracy_score')
 ax.semilogx()
 ax.set_ylim(np.min(acc_ete_scratch)-0.001,1)
 ax.grid(True)
-ax.legend(loc='upper left')
-ax.set_xlabel(r'# Training Data',loc="right")
+ax.legend(loc='lower right')
+ax.set_xlabel(r'# Training data (evts)',loc="right")
 fig.savefig(f"../../Finetune_hep/plots/Subsets.pdf")
 
