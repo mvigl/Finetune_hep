a480d02f1e51aa59bad16a1a7291b40704b2fb0f
mavigl <matthias.vigl@ern.ch>
2023-12-06 14:34:53 +0100

    scratch scalar

--- save_ParTXbb.py
+++ save_ParTXbb.py
@@ -47,18 +47,18 @@ threshold=[]
 
 ParTXbb_model.to(device)
 ParTXbb_model.eval()
-#ParTXbb_model = df.load_Xbb_backbone(ParTXbb_model,'Xbb',mlp_layers=1,ParT_params_path=model_path,mlp_params_path='no')
-ParTXbb_model.load_state_dict(torch.load(model_path))   
+ParTXbb_model = df.load_Xbb_backbone(ParTXbb_model,'Xbb',mlp_layers=1,ParT_params_path=model_path,mlp_params_path='no')
+#ParTXbb_model.load_state_dict(torch.load(model_path))   
 
 print('device: ', device)
 
-out_dir = f'/raven/u/mvigl/Finetune_hep_dir/Finetune_hep/models/ParTXbb_double/{name}/'
+out_dir = f'/raven/u/mvigl/Finetune_hep_dir/Finetune_hep/models/ParTXbb_scratch_etoe/{name}/'
 yi_ParTXbb = ParT_mlp.get_Xbb_preds(ParTXbb_model,filelist_train,device,out_dir,Xbb=True)
 
-out_dir = f'/raven/u/mvigl/Finetune_hep_dir/Finetune_hep/models/ParTXbb_double/{name}/'
+out_dir = f'/raven/u/mvigl/Finetune_hep_dir/Finetune_hep/models/ParTXbb_scratch_etoe/{name}/'
 yi_ParTXbb = ParT_mlp.get_Xbb_preds(ParTXbb_model,filelist_test,device,out_dir,Xbb=True)
 
-out_dir = f'/raven/u/mvigl/Finetune_hep_dir/Finetune_hep/models/ParTXbb_double/{name}/'
+out_dir = f'/raven/u/mvigl/Finetune_hep_dir/Finetune_hep/models/ParTXbb_scratch_etoe/{name}/'
 yi_ParTXbb = ParT_mlp.get_Xbb_preds(ParTXbb_model,filelist_val,device,out_dir,Xbb=True)
 
 
