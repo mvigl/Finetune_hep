de6d2073baa1d2d63599b1623ab0980c9e6098ff
mavigl <matthias.vigl@ern.ch>
2024-02-12 11:07:30 +0100

    save scores

--- save_scores.py
+++ save_scores.py
@@ -1,9 +1,8 @@
 from comet_ml import Experiment,ExistingExperiment
 from comet_ml.integration.pytorch import log_model
 from Finetune_hep.python import train,helpers,models
-import torch
 import argparse
-import yaml
+import os
 
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('--out', help='message',default='/raven/u/mvigl/public/run/Xbb/scores/')
@@ -16,6 +15,7 @@ args = parser.parse_args()
 device = helpers.get_device()
 model = models.full_model(args.config,save_representaions=False,for_inference=True)
 model = helpers.load_weights(model,args.checkpoint,device)
+if (not os.path.exists(args.out)): os.system(f'mkdir {args.out}')
 
 if __name__ == '__main__':    
 
