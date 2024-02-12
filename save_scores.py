bb807e8dc216f55b615c71ccc00162ff721892cb
mavigl <matthias.vigl@ern.ch>
2024-02-12 14:16:52 +0100

    save scores

--- save_scores.py
+++ save_scores.py
@@ -7,7 +7,7 @@ import os
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('--out', help='message',default='/raven/u/mvigl/public/run/Xbb/scores')
 parser.add_argument('--config', help='config',default='/raven/u/mvigl/public/Finetune_hep/config/ParT_Xbb_config.yaml')
-parser.add_argument('--data', help='data',default='/raven/u/mvigl/Finetune_hep_dir/config/train_list.txt')
+parser.add_argument('--data', help='data',default='/raven/u/mvigl/Finetune_hep_dir/config/val_list.txt')
 parser.add_argument('--checkpoint',  help='training-checkpoint',default='/raven/u/mvigl/public/run/Xbb/models/Xbb_lr0.01_bs512_subset0.1.pt')
 parser.add_argument('--repDim', type=int,  help='repDim',default='1')
 
