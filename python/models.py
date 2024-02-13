b9177c3a4eb00cfc1651144f9313a8842edd8cbc
mavigl <matthias.vigl@ern.ch>
2024-02-13 16:00:39 +0100

    save scores

--- python/models.py
+++ python/models.py
@@ -1,10 +1,12 @@
-from Finetune_hep.python import ParT
+from Finetune_hep.python import ParT,ParT_hlf,helpers,train,head
 from Finetune_hep.python import ParT_hlf
 from Finetune_hep.python import helpers,train
+from sklearn.preprocessing import StandardScaler
 import torch
 import numpy as np
 import h5py
 import yaml
+import pickle
 
 def full_model(config_file, **kwargs):
 
@@ -83,3 +85,66 @@ def save_rep(model,device,filelist,out_dir,repDim):
                         Data.create_dataset('evt_label', data=target.reshape(-1),dtype='i4')
                         Data.close()      
     return 0
+
+def save_rep_head(model,device,filelist,out_dir,repDim,Xbb_scores_path,use_hlf,scaler_path):
+
+    with torch.no_grad():
+        model.to(device)
+        model.eval()
+        with open(filelist) as f:
+            for line in f:
+                filename = line.strip()
+                print('reading : ',filename)
+                with h5py.File(filename, 'r') as Data:
+                    subset_offset = int(len(Data['X_jet']))
+                    if i ==0:
+                        hlf = Data['X_jet'][:subset_offset]
+                        target = Data['labels'][:subset_offset] 
+                        jet_mask = Data['jet_mask'][:subset_offset]
+                    else:
+                        hlf = np.concatenate((hlf,Data['X_jet'][:subset_offset]),axis=0)
+                        target = np.concatenate((target,Data['labels'][:subset_offset]),axis=0)
+                        jet_mask = np.concatenate((jet_mask,Data['jet_mask'][:subset_offset]),axis=0)
+                    i+=1    
+        scaler = StandardScaler()
+        hlf[:,:,helpers.jVars.index('fj_pt')] = helpers.log(hlf[:,:,helpers.jVars.index('fj_pt')])
+        hlf[:,:,helpers.jVars.index('fj_mass')] = helpers.log(hlf[:,:,helpers.jVars.index('fj_mass')])
+        hlf[:,:,helpers.jVars.index('fj_sdmass')] = helpers.log(hlf[:,:,helpers.jVars.index('fj_sdmass')])
+        if Xbb_scores_path != '': 
+            subset_offset=0
+            i=0
+            with open(Xbb_scores_path) as f:
+                for line in f:
+                    filename = line.strip()
+                    print('loading Xbb scores from : ',filename)
+                    with h5py.File(filename, 'r') as Xbb_scores:
+                        subset_offset = int(len(Xbb_scores['Xbb_score']))
+                        if i ==0:
+                            Xbb = Xbb_scores['Xbb_score'][:subset_offset]
+                        else:
+                            Xbb = np.concatenate((Xbb,Xbb_scores['Xbb_score'][:subset_offset]),axis=0)
+                        i+=1    
+            if repDim != 1: hlf[:,:,helpers.jVars.index('fj_doubleb')] = np.nan_to_num(Xbb.reshape(-1,5))  
+                    
+        if repDim != 1: Xbb = hlf[:,:,helpers.jVars.index('fj_doubleb')].reshape(-1,5,1)
+        
+        hlfeats = [helpers.jVars.index('fj_pt'),helpers.jVars.index('fj_eta'),helpers.jVars.index('fj_phi'),helpers.jVars.index('fj_mass'),helpers.jVars.index('fj_sdmass')]
+        if use_hlf: data = np.concatenate((Xbb,hlf[:,:,hlfeats]),axis=-1)      
+        else: data = Xbb
+
+        if scaler_path !='' : 
+            with open(scaler_path,'rb') as f:
+                scaler = pickle.load(f)
+                X_norm = head.transform_without_zeros(data,jet_mask,scaler)
+                x = torch.from_numpy(X_norm).float().to(device)
+        else:
+            x = torch.from_numpy(data).float().to(device)    
+        y = torch.from_numpy(target.reshape(-1,1)).float().to(device)
+        jet_mask = torch.from_numpy(jet_mask.reshape(-1,5,1)).float().to(device) 
+        preds = model(x) 
+
+        Data = h5py.File(out_dir, 'w')
+        Data.create_dataset('evt_score', data=preds.reshape(-1,repDim))
+        Data.create_dataset('evt_label', data=y.reshape(-1),dtype='i4')
+        Data.close()   
+        
--- save_scores.py
+++ save_scores.py
@@ -3,17 +3,21 @@ import argparse
 import os
 
 parser = argparse.ArgumentParser(description='')
-parser.add_argument('--out', help='message',default='/raven/u/mvigl/public/run/Scratch_Xbb_hl/scores')
+parser.add_argument('--out', help='message',default='/raven/u/mvigl/public/run/Frozen_Xbb_hl/scores')
 parser.add_argument('--config', help='config',default='/raven/u/mvigl/public/Finetune_hep/config/ParT_Xbb_hlf_config.yaml')
 parser.add_argument('--data', help='data',default='/raven/u/mvigl/Finetune_hep_dir/config/test_list.txt')
-parser.add_argument('--checkpoint',  help='training-checkpoint',default='/raven/u/mvigl/public/run/Scratch_Xbb_hl/models/Scratch_Xbb_hl_lr0.001_bs256_subset0.1.pt')
+parser.add_argument('--checkpoint',  help='training-checkpoint',default='/raven/u/mvigl/public/run/Frozen_Xbb_hl/models/Frozen_Xbb_hl_lr0.001_bs512_subset0.1.pt')
 parser.add_argument('--repDim', type=int,  help='repDim',default='1')
 parser.add_argument('--save_representaions',  action='store_true', help='save_representaions', default=False)
+parser.add_argument('--Xbb', help='data',default='/raven/u/mvigl/public/Finetune_hep/config/Xbb_test_list.txt')
+parser.add_argument('--scaler_path',  help='training-checkpoint',default='')
+parser.add_argument('--use_hlf',  action='store_true', help='use_hlf', default=True)
 
 args = parser.parse_args()
 
 device = helpers.get_device()
-model = models.full_model(args.config,save_representaions=args.save_representaions,for_inference=True)
+if args.head: model = models.head(args.config,save_representaions=args.save_representaions,for_inference=True)
+else: model = models.full_model(args.config,save_representaions=args.save_representaions,for_inference=True)
 model = helpers.load_weights(model,args.checkpoint,device)
 if (not os.path.exists(args.out)): os.system(f'mkdir {args.out}')
 
@@ -21,4 +25,6 @@ if __name__ == '__main__':
 
     print(model)
     model.to(device)
-    models.save_rep(model,device,args.data,args.out,args.repDim)
+    if args.head: models.save_rep_head(model,device,args.data,args.out,args.repDim,args.Xbb,args.use_hlf,args.scaler_path)
+    else: models.save_rep(model,device,args.data,args.out,args.repDim)
+    
