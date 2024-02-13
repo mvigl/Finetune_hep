a8c4238b9774723065306341fb3a044e6bed66c8
mavigl <matthias.vigl@ern.ch>
2024-02-13 15:34:29 +0100

    save scores

--- save_scores.py
+++ save_scores.py
@@ -3,10 +3,10 @@ import argparse
 import os
 
 parser = argparse.ArgumentParser(description='')
-parser.add_argument('--out', help='message',default='/raven/u/mvigl/public/run/Finetuned_Xbb_hl/scores')
+parser.add_argument('--out', help='message',default='/raven/u/mvigl/public/run/Scratch_Xbb_hl/scores')
 parser.add_argument('--config', help='config',default='/raven/u/mvigl/public/Finetune_hep/config/ParT_Xbb_hlf_config.yaml')
 parser.add_argument('--data', help='data',default='/raven/u/mvigl/Finetune_hep_dir/config/test_list.txt')
-parser.add_argument('--checkpoint',  help='training-checkpoint',default='/raven/u/mvigl/public/run/Finetuned_Xbb_hl/models/Finetuned_Xbb_hl_lr0.001_bs256_subset0.1.pt')
+parser.add_argument('--checkpoint',  help='training-checkpoint',default='/raven/u/mvigl/public/run/Scratch_Xbb_hl/models/Scratch_Xbb_hl_lr0.001_bs256_subset0.1.pt')
 parser.add_argument('--repDim', type=int,  help='repDim',default='1')
 parser.add_argument('--save_representaions',  action='store_true', help='save_representaions', default=False)
 
